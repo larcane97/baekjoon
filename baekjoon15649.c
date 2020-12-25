ad74f5dcbac326f313cbf73f6262a16abcdf67ba
larcane97 <70624819+larcane97@users.noreply.github.com>
2020-12-25 17:10:49 +0900

    baekjoon15649 : N과M(1)

---
+++ baekjoon15649.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#define false 0
+#define true 1
+#define MAXLEN 9
+
+int ans[MAXLEN];
+int len;
+
+void f(int* arr, int n, int m) {
+	if (m >= 1) {
+		for (int i = 1; i <= n; i++) {
+			if (arr[i]) {
+				arr[i] = false;
+				ans[len++] = i;
+				//printf("%d ", i);
+				f(arr, n, m - 1);
+				len--;
+				arr[i] = true;
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < len; i++)
+			printf("%d ", ans[i]);
+		printf("\n");
+	}
+}
+
+
+
+int main() {
+	int n, m, i, j;
+	scanf("%d %d", &n, &m);
+	int* arr = (int*)malloc(sizeof(int) * (n + 1));
+	for (i = 1; i <= n; i++)
+		arr[i] = true;
+	f(arr,n, m);
+}
