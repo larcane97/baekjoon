299f3a3a6d007721a7e2e9bfb363d1ab6aee2a47
larcane97 <70624819+larcane97@users.noreply.github.com>
2021-01-13 00:23:19 +0900

    baekjoon9184.c : 신나는 함수 실행

---
+++ baekjoon9184.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int w[21][21][21];
+
+int main() {
+	int i, j, k;
+	for (i = 0; i < 21; i++) {
+		for (j = 0; j < 21; j++) {
+			for (k = 0; k < 21; k++) {
+				if (i == 0 || j == 0 || k == 0)
+					w[i][j][k] = 1;
+			}
+		}
+	}
+
+	for (i = 0; i < 21; i++) {
+		for (j = 0; j < 21; j++) {
+			for (k = 0; k < 21; k++) {
+				if (i > 20 || j > 20 || k > 20)
+					w[i][j][k] = w[20][20][20];
+				else if (i!=0&j!=0&k!=0) {
+					if (i < j && j < k)
+						w[i][j][k] = w[i][j][k - 1] + w[i][j - 1][k - 1] - w[i][j - 1][k];
+					else
+						w[i][j][k] = w[i - 1][j][k] + w[i - 1][j-1][k] + w[i-1][j][k-1]- w[i - 1][j - 1][k - 1];
+				}
+					
+			}
+		}
+	}
+	int a, b, c;
+	while(1) {
+		scanf("%d %d %d", &a, &b, &c);
+		if (a == -1 && b == -1 && c == -1)
+			break;
+		if (a <= 0 || b <= 0 || c <= 0)
+			printf("w(%d, %d, %d) = %d\n", a,b,c,w[0][0][0]);
+		else if(a>20 || b>20 || c>20)
+			printf("w(%d, %d, %d) = %d\n",a,b,c,w[20][20][20]);
+		else
+			printf("w(%d, %d, %d) = %d\n",a,b,c,w[a][b][c]);
+	}
+
+
+}
