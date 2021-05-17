54eaf40a2759f7ad6205341e5db605330f2da446
larcane97 <70624819+larcane97@users.noreply.github.com>
2021-05-17 14:34:08 +0900

    baekjoon1463 : 1로 만들기

--- baekjoon1463.cpp
+++ baekjoon1463.cpp
@@ -1,2 +1,31 @@
 #include <iostream>
+using namespace std;
 
+
+int* mem = new int[1000001]{ 0 };
+int n;
+
+int solve(int num,int calc) {
+	if (num == 1) return 0;
+
+	int& ret = mem[num];
+	if (ret != 0) return ret;
+
+	int tmp = 987654321;
+	if (num % 3 == 0)
+		tmp = min(tmp,1+ solve(num/ 3, calc + 1));
+	if (num % 2 == 0)
+		tmp = min(tmp, 1+solve(num / 2, calc + 1));
+
+	return ret =min(tmp, 1+solve(num - 1, calc + 1));
+}
+
+
+int main() {
+	cin >> n;
+	cout << solve(n, 0) << endl;
+
+	delete[] mem;
+	return 0;
+
+}
