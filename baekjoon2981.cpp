06ea75cf7e515f38dfe6094e8c23b6a81b80d40c
larcane97 <70624819+larcane97@users.noreply.github.com>
2021-05-25 18:25:03 +0900

    baekjoon2981 : 검문(정수론)

--- baekjoon2981.cpp
+++ baekjoon2981.cpp
@@ -1,12 +1,46 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
 typedef long long ll;
 using namespace std;
 
-ll arr[101];
+vector<ll> arr;
 int n;
-int main() {
+
+ll gcd(ll a, ll b) {
+	if(a < b) swap(a, b);
+	return a % b == 0 ? b : gcd(b, a % b);
+}
+
+void solve() {
+	vector<ll>ret;
+	ll GCD = abs(arr[1] - arr[0]);
+	for (int i = 2; i < arr.size(); i++) {
+		GCD = gcd(GCD, abs(arr[i] - arr[i - 1]));
+	}
+	for (int i = 2; i <=sqrt(GCD); i++) {
+		if (GCD % i == 0) {
+			ret.push_back(i);
+			ret.push_back(GCD / i);
+		}
+	}
+	ret.push_back(GCD);
+	sort(ret.begin(), ret.end());
+	ret.erase(unique(ret.begin(), ret.end()), ret.end());
+	for (int i = 0; i < ret.size(); i++)
+		cout << ret[i] << " ";
+}
+
+void input() {
 	cin >> n;
+	arr = vector<ll>(n);
 	for (int i = 0; i < n; i++)
 		cin >> arr[i];
+}
+
+int main() {
+	input();
+	solve();
 
 }
